08c36e962b1532713b7cfa84cb61f9e6eac8f38d
Arto Bendiken <arto@conreality.org>
2017-06-21 19:57:26 +0200

    Implemented Client#connect() and #disconnect().

--- src/conreality/sdk/client.py
+++ src/conreality/sdk/client.py
@@ -1,26 +1,43 @@
 # This is free and unencumbered software released into the public domain.
 
 import asyncio
+import asyncpg
 
 class AsyncClient:
     """An asynchronous client connection."""
 
     def __init__(self):
-        pass
+        self.conn = None
 
-    async def answer(self):
-        await asyncio.sleep(3)
-        return 42
+    async def connect(self, **kwargs):
+        self.conn = await asyncpg.connect(**kwargs)
+        return self.conn
+
+    async def disconnect(self):
+        if self.conn is not None:
+            await self.conn.close()
+            self.conn = None
+
+    async def execute(self, query_text):
+        assert self.conn is not None
+        return await self.conn.fetch(query_text)
 
 class Client(AsyncClient):
     """A synchronous client connection."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__()
         self.loop = asyncio.get_event_loop()
+        self.connect(**kwargs)
 
     def wait(self, future):
         return self.loop.run_until_complete(future)
 
-    def answer(self):
-        return self.wait(super().answer())
+    def connect(self, **kwargs):
+        return self.wait(super().connect(**kwargs))
+
+    def disconnect(self):
+        return self.wait(super().disconnect())
+
+    def execute(self, query_text):
+        return self.wait(super().execute(query_text))
