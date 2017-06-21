5a9efcac026380ba50205e4504ddbde8b4783b46
Arto Bendiken <arto@conreality.org>
2017-06-21 16:41:41 +0200

    Added a test suite placeholder.

---
+++ test/test_package.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# This is free and unencumbered software released into the public domain.
+
+"""Test cases for the conreality.sdk module."""
+
+from conreality.sdk import *
+
+class Test: # TODO
+  """Test cases for the conreality.sdk module.""" # TODO
+
+  def test_construction(self):
+    assert True # TODO
+
+if __name__ == '__main__':
+  import pytest, sys
+  sys.exit(pytest.main(__file__))
